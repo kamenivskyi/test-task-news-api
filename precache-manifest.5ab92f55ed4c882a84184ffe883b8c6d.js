a98994fd7ee1827b34ae4aef55f09a141c6df991
Roman Kamenivskyi <jumji19992412@gmail.com>
2020-03-21 19:58:08 +0200

    Updates

---
+++ precache-manifest.5ab92f55ed4c882a84184ffe883b8c6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab0ee4e2dd0a5df89a70211d3e7a569b",
+    "url": "/test-task-news-api/index.html"
+  },
+  {
+    "revision": "aaf804c799827ddef4a9",
+    "url": "/test-task-news-api/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "36496c94f870712fca2e",
+    "url": "/test-task-news-api/static/css/main.fd83123a.chunk.css"
+  },
+  {
+    "revision": "aaf804c799827ddef4a9",
+    "url": "/test-task-news-api/static/js/2.343021da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-task-news-api/static/js/2.343021da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36496c94f870712fca2e",
+    "url": "/test-task-news-api/static/js/main.49ba2dda.chunk.js"
+  },
+  {
+    "revision": "9251196a31d1d19a378c",
+    "url": "/test-task-news-api/static/js/runtime-main.94833301.js"
+  }
+]);
