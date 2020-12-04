a90592a67e75983d4130bbafd027adcc91bb4f73
Roman Kamenivskyi <jumji19992412@gmail.com>
2020-12-04 18:11:12 +0200

    add prettier extension for project

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,29 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 
-import { setAuthStatusTrue } from '../../redux/auth/authActions';
+import { setAuthStatusTrue } from "../../redux/auth/authActions";
+import { getItemFromStorage, setItemToStorage } from "../../utils/localStorage";
 
-import {
-  getItemFromStorage,
-  setItemToStorage
-} from '../../utils/localStorage';
-
-import './Login.css';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import "./Login.css";
 
 const Login = () => {
-  const [formState, setFormState] = useState({ name: '', password: '' });
+  const [formState, setFormState] = useState({ name: "", password: "" });
   const dispatch = useDispatch();
 
   const { isLoggedIn } = useSelector(({ authData }) => authData);
   const { name, password } = formState;
 
   useEffect(() => {
-    if (getItemFromStorage('isAuthorized')) {
-    
+    if (getItemFromStorage("isAuthorized")) {
       dispatch(setAuthStatusTrue());
     }
   }, []);
@@ -31,42 +23,39 @@ const Login = () => {
   const handleInputChange = ({ target: { name, value } }) => {
     setFormState({
       ...formState,
-      [name]: value
+      [name]: value,
     });
-  };  
+  };
 
-  const handleSubmitForm = event => {
+  const handleSubmitForm = (event) => {
     event.preventDefault();
     checkAuthData(name, password);
   };
 
   const checkAuthData = (name, password) => {
-    if (name === 'admin' && password === '12345') {
-      setItemToStorage('isAuthorized', true);
+    if (name === "admin" && password === "12345") {
+      setItemToStorage("isAuthorized", true);
       dispatch(setAuthStatusTrue());
 
-      setFormState({ ...formState, name: '', password: '' });
+      setFormState({ ...formState, name: "", password: "" });
     } else {
-      alert('The username or password you entered is incorrect');
+      alert("The username or password you entered is incorrect");
     }
   };
 
-    if (isLoggedIn) {
-      return <Redirect to='/profile' />;
-    }
+  if (isLoggedIn) {
+    return <Redirect to="/profile" />;
+  }
 
-    return (
-      <form
-        className='login-form'
-        onSubmit={handleSubmitForm}
-      >
-        <h1 className='login-title'>Please sign in</h1>
-
+  return (
+    <>
+      <h1 className="login-title">Please sign in</h1>
+      <form className="login-form" onSubmit={handleSubmitForm}>
         <input
-          type='text'
-          className='form-control'
-          placeholder='Username'
-          name='name'
+          type="text"
+          className="form-control"
+          placeholder="Username"
+          name="name"
           autoFocus={true}
           value={name}
           onChange={handleInputChange}
@@ -74,18 +63,19 @@ const Login = () => {
         />
 
         <input
-          type='password'
-          className='form-control my-3'
-          placeholder='Password'
-          name='password'
+          type="password"
+          className="form-control my-3"
+          placeholder="Password"
+          name="password"
           value={password}
           onChange={handleInputChange}
           required
         />
-        <button className='btn btn-lg btn-dark btn-block' type='submit'>
+        <button className="btn btn-lg btn-dark btn-block" type="submit">
           Sign in
         </button>
       </form>
-    );
-}
+    </>
+  );
+};
 export default Login;
